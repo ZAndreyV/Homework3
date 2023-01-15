115067a88f8e9c9bd615430d82be8ff7d6578a94
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2023-01-15 17:40:11 +0700

    Addad Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 27: Напишите программу, которая принимает 
+на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12 */
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sumDigitsNumber = SumDigitsNumber(number);
+Console.WriteLine($"The sum of the digits in a number: {number} -> {sumDigitsNumber}");
+
+int SumDigitsNumber(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result = (num%10) + result;
+        num /=10;
+
+    }
+    return result;
+
+} 
+
+
+
